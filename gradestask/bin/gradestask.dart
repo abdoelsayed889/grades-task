0eabf6212dec3aabe2e0b8c105494bc05756ba1e
abdelrahman-gif <abdoelsayed889@gmail.com>
2021-03-22 23:45:58 +0200

    grades task

---
+++ gradestask/bin/gradestask.dart
@@ -0,0 +1,27 @@
+void main()
+{
+  var grade=40;
+
+  if (grade >= (50) && grade <(60))
+  {
+    print('D');
+  }
+   else if (grade >= (60) && grade <(70))
+   {
+     print('C');
+   }
+   else if (grade >=70 && grade < 80)
+     {
+       print('B');
+     }
+   else if (grade >=80 && grade <90)
+     {
+       print('A');
+     }
+   else if(grade >=90 && grade <=100)
+     {
+       print('A+');
+     }
+   else
+    print('F');
+}
---
+++ gradestask/test/gradestask_test.dart
@@ -0,0 +1,8 @@
+import 'package:gradestask/gradestask.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
